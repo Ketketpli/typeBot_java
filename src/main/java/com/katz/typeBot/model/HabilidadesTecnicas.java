fac3547f9ca1641a91c08f7ef9060fecbce70e2f
Paulo Henrique <varelapaulo852@gmail.com>
2026-06-17 19:11:21 -0300

    Updated habilidades

---
+++ src/main/java/com/katz/typeBot/model/HabilidadesTecnicas.java
@@ -0,0 +1,28 @@
+package com.katz.typeBot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "habilidades")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HabilidadesTecnicas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "perfil_id")
+    private Perfil perfil;
+
+    @Column
+    private String habilidades;
+    private String proeficiencia;
+}
