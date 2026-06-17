71183f737a3f354c6e362190defaa0dc0fe54101
Paulo Henrique <varelapaulo852@gmail.com>
2026-06-17 19:11:43 -0300

    Creating idiomas

---
+++ src/main/java/com/katz/typeBot/model/Idiomas.java
@@ -0,0 +1,27 @@
+package com.katz.typeBot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "idiomas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Idiomas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "perfil_id")
+    private Perfil perfil;
+
+    private String nomeIdiomas;
+    private String proeficiencia;
+}
