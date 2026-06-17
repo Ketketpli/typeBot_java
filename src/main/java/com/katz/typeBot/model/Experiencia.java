d93929f0343c90239a8130bab951f23a05733374
Paulo Henrique <varelapaulo852@gmail.com>
2026-06-17 19:32:37 -0300

    Creating experiencia

---
+++ src/main/java/com/katz/typeBot/model/Experiencia.java
@@ -0,0 +1,33 @@
+package com.katz.typeBot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "experiencia")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Experiencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "perfil_id")
+    private Perfil perfil;
+
+    @Column(name = "nome_empresa")
+    private String empresa;
+
+    @Column(name = "data_inicio")
+    private String dataInicio;
+
+    @Column(name = "data_fim")
+    private String dataFim;
+}
