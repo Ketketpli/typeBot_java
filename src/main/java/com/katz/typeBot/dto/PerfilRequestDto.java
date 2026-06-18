ec8af9555cf5e2da08bf69edeccb9c1b13014bd7
Paulo Henrique <varelapaulo852@gmail.com>
2026-06-18 19:58:44 -0300

    Updating PerfilDTO & repository

---
+++ src/main/java/com/katz/typeBot/dto/PerfilRequestDto.java
@@ -0,0 +1,13 @@
+package com.katz.typeBot.dto;
+
+import java.math.BigDecimal;
+
+public record PerfilRequestDto(String nome,
+                               String email,
+                               String telefone,
+                               String linkedin,
+                               String curriculo,
+                               String endereco,
+                               String senioridade,
+                               BigDecimal pretensaoSalarial,
+                               String pcd) {}
---
+++ src/main/java/com/katz/typeBot/dto/PerfilResponseDto.java
@@ -0,0 +1,5 @@
+package com.katz.typeBot.dto;
+
+public record PerfilResponseDto(String nome,
+                                String email,
+                                String telefone) {}
